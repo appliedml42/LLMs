ad6b82f8bae6839012881e702d1f97f604eec7b8
appliedml85 <appliedml85@gmail.com>
2022-02-20 09:06:31 -0800

    Add Pile DataModule

--- src/data/pile.py
+++ src/data/pile.py
@@ -1,75 +1,88 @@
-import argparse
-import logging
+import json
 import os
-from argparse import Namespace
+from random import shuffle
+from typing import Optional
+
+import numpy as np
 import sentencepiece as spm
-import pandas as pd
 import torch
-from torch.utils.data import Dataset
+from pytorch_lightning import LightningDataModule
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
+from torch.utils.data import IterableDataset, DataLoader
 
 
-class Pile(Dataset):
-    def __init__(self, *args):
-        if len(args) == 2:
-            generic_args, other_args = args
-            self.parser = argparse.ArgumentParser()
-            self.parser.add_argument('--dpath', type=str, required=True)
-            self.params, _ = self.parser.parse_known_args(other_args, namespace=generic_args)
-            self._initialize()
-        elif isinstance(args[0], Namespace):
-            self.params = args[0]
-
-        self.tokenizer = None
-
-    def _initialize(self):
-        self.dataset = pd.read_json(os.path.join(self.params.dpath, 'df.json'), lines=True).reset_index()
-        self.image_size = self.params.image_size
-        self.text_seq_len = self.params.text_seq_len
+class PileDataset(IterableDataset):
+    def __init__(self, fpaths, seq_len, tokenizer):
+        self.fpaths = fpaths
+        self.seq_len = seq_len - 2
         self.tokenizer = tokenizer
-        self.params.vocab_size = self.tokenizer.vocab_size
-        self.transform = T.Compose([
-            T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
-            T.Resize((self.image_size, self.image_size)),
-            T.ToTensor()
-        ])
 
-    def __len__(self):
-        return len(self.dataset)
+    def __iter__(self):
+        worker_info = torch.utils.data.get_worker_info()
+        files_per_worker = int(len(self.fpaths) / worker_info.num_workers)
+        start_idx = worker_info.id * files_per_worker
+        end_idx = start_idx + files_per_worker if worker_info.id < worker_info.num_workers - 1 else len(self.fpaths)
+        fpaths = self.fpaths[start_idx:end_idx]
+        shuffle(fpaths)
 
-    def tokenize(self, text):
-        return self.tokenizer.tokenize(text,
-                                       self.text_seq_len,
-                                       truncate_text=True)
+        for fpath in fpaths:
+            with open(fpath) as reader:
+                for line in reader:
+                    text = json.loads(line)['text']
+                    ids = self.tokenizer.encode_as_ids(text)
+                    for i in range(0, len(ids), self.seq_len):
+                        if len(ids) == self.seq_len:
+                            seq = [self.tokenizer.bos_id()] + ids[i:i + self.seq_len] + [self.tokenizer.eos_id()]
+                        else:
+                            seq = ids[i:i + self.seq_len]
+                            seq = [self.tokenizer.bos_id()] + seq + [self.tokenizer.eos_id()]
+                            seq = seq + [self.tokenizer.pad_id()] * (self.seq_len + 2 - len(seq))
+                        yield np.asarray(seq)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
 
-        img = None
-        img_fpath = self.dataset.iloc[idx, 1]
-        caption_text = self.dataset.iloc[idx, 2]
-        bird = self.dataset.iloc[idx, 3]
-        while img is None:
-            try:
-                with open(img_fpath, 'rb') as fpath:
-                    img = Image.open(fpath)
-                    img = self.transform(img)
-                    caption = self.tokenize(caption_text).squeeze(0)
-            except Exception as e:
-                img = None
-                rand_sample = self.dataset.sample()
-                caption_text = rand_sample['caption'].values[0]
-                img_fpath = rand_sample['image_fpath'].values[0]
-                if self.params.gen_logs:
-                    logging.error(e)
-                    raise e
+class Pile(LightningDataModule):
+    def __init__(self, seq_len, batch_size, tokenizer_path, path, num_workers):
+        self.tokenizer = spm.SentencePieceProcessor()
+        self.tokenizer.load(tokenizer_path)
+        self.vocab_size = self.tokenizer.vocab_size()
 
-        if self.params.gen_logs:
-            logging.info(f'idx:{idx} fpath:{img_fpath} bird:{bird}')
+        self.seq_len = seq_len
 
-        return {
-            'image': img,
-            'caption': caption,
-            'mask': caption != 0,
-            'caption_text': caption_text
-        }
+        self.batch_size = batch_size
+
+        self.num_workers = num_workers
+
+        self.path = path
+
+        super().__init__()
+        self.save_hyperparameters()
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        if stage == 'fit':
+            train_path = os.path.join(self.path, 'fit', 'train')
+            val_path = os.path.join(self.path, 'fit', 'val')
+
+            self.train_paths = [os.path.join(train_path, x) for x in os.listdir(train_path) if x.endswith('jsonl')]
+            shuffle(self.train_paths)
+            self.train_dataset = PileDataset(self.train_paths, self.seq_len, self.tokenizer)
+
+            self.val_paths = [os.path.join(val_path, x) for x in os.listdir(val_path) if x.endswith('jsonl')]
+            shuffle(self.val_paths)
+            self.val_dataset = PileDataset(self.val_paths, self.seq_len, self.tokenizer)
+
+        if stage == 'test':
+            self.test_paths = [os.path.join(self.path, x) for x in os.listdir(self.path) if x.endswith('jsonl')]
+            shuffle(self.path)
+            self.test_dataset = PileDataset(self.path, self.seq_len, self.tokenizer)
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        raise ValueError('No prediction dataloader implemented')
